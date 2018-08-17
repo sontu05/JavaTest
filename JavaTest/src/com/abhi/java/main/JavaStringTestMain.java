13a65788da3502b003ff3b855f8c465b7be9c34f
shayanipal <shayanipal@unknownd8bb2cb83dc4>
2018-08-17 17:03:14 +0200

    added some more code String buffer and Builder.

--- JavaTest/src/com/abhi/java/main/JavaStringTestMain.java
+++ JavaTest/src/com/abhi/java/main/JavaStringTestMain.java
@@ -5,6 +5,12 @@ public class JavaStringTestMain {
 	public static void main(String[] args) {
 		String name1= new String ("Test");
 		System.out.println("name1 ="+name1);
+		
+		StringBuffer nameSbuff= new StringBuffer("TestSBuff");
+		System.out.println("nameSbuff ="+nameSbuff);
+		
+		StringBuilder nameSbld= new StringBuilder("TestSBuff");
+		System.out.println("nameSbld ="+nameSbld);
 
 	}
 
